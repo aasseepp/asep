3887e75538a8f4958adfc820e2bafb9d12437c2e
user <user@192.168.10.133>
2012-06-21 18:53:26 +0700

    pesan dengan metode static

--- asep/src/asep/Asep.java
+++ asep/src/asep/Asep.java
@@ -15,6 +15,6 @@ public class Asep {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("selamat datang ");
+        Pesan.tampilPesan();
     }
 }
---
+++ asep/src/asep/Pesan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asep;
+
+/**
+ *
+ * @author user
+ */
+public class Pesan {
+
+    public Pesan() {
+    }
+    public static void tampilPesan(){
+        String pesan="Selamat datang";
+        System.out.println(pesan);
+    }
+}
