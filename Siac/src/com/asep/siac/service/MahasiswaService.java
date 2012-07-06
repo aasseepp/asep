b6d6ab6cf75e07b8a0675d80f0acb711d00e83a9
user <user@192.168.10.133>
2012-07-06 20:04:05 +0700

    Aplikasi sederhana Akademik

--- Siac/src/com/asep/siac/service/MahasiswaService.java
+++ Siac/src/com/asep/siac/service/MahasiswaService.java
@@ -4,27 +4,29 @@
  */
 package com.asep.siac.service;
 
+
 import com.asep.siac.entity.Mahasiswa;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
- * @author user
+ * @author mahasiswa
  */
 public class MahasiswaService {
 
     public MahasiswaService() {
     }
-    private final String INSERT_SQL="INSERT INTO mahasiswa (nim, nama, tempat_lahir, tgl_lahir, telepon, alamat) VALUES (?, ?, ?, ?, ?, ?)";
-    private final String UPDATE_SQL="UPDATE mahasiswa SET nim=?, nama=?, tempat_lahir=?, tgl_lahir=?, telepon=?, alamat=? where nim=? ";
-    private final String DELETE_SQL="DELETE FROM mahasiswa WHERE nim=?";
-    private final String SELECT_SQL="SELECT nim, nama, tempat_lahir, tgl_lahir, tetepon, alamat FROM mahasiswa limit ?, ?";
-    private final String SELECT_ONE_SQL="SELECT nim, nama, tempat_lahir, tgl_lahir, tetepon, alamat FROM mahasiswa WHERE nim=?";
-    private final String COUNT_SQL="SELECT COUNT(nim) AS total FROM mahasiswa";
+    private final String INSERT_SQL="insert into mahasiswa (nim, nama, tempat_lahir, tgl_lahir, telepon, alamat) values (?, ?, ?, ?, ?, ?)";
+    private final String UPDATE_SQL="update mahasiswa set nama=?, tempat_lahir=?, tgl_lahir=?, telepon=?, alamat=? where nim=?";
+    private final String DELETE_SQL="delete from mahasiswa where nim=?";
+    private final String SELECT_SQL="select nim, nama, tempat_lahir, tgl_lahir, telepon, alamat from mahasiswa limit ?, ? ";
+    private final String COUNT_SQL="select count(nim) as total from mahasiswa ";
+    private final String SELECT_ONE_SQL="select nim, nama, tempat_lahir, tgl_lahir, telepon, alamat from mahasiswa where nim like ?";
     
     public void insert(Mahasiswa mahasiswa){
         try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(INSERT_SQL)){
@@ -35,93 +37,94 @@ public class MahasiswaService {
             statement.setString(5, mahasiswa.getTelepon());
             statement.setString(6, mahasiswa.getAlamat());
             statement.executeUpdate();
-        }catch(Exception e){
-            e.getMessage();
-        }
-    } 
-    
-     public void update(Mahasiswa mahasiswa){
-        try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(UPDATE_SQL)){
-            statement.setString(1, mahasiswa.getNim());
-            statement.setString(2, mahasiswa.getNama());
-            statement.setString(3, mahasiswa.getTemptLahir());
-            statement.setDate(4, new Date(mahasiswa.getTglLahir().getTime()));
-            statement.setString(5, mahasiswa.getTelepon());
-            statement.setString(6, mahasiswa.getAlamat());
-            statement.setString(7, mahasiswa.getNim());
-            statement.executeUpdate();
-        }catch(Exception e){
-            e.getMessage();
-        }
-    } 
-     
-     public void delete(Mahasiswa mahasiswa){
-        try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(DELETE_SQL)){
-            statement.setString(1, mahasiswa.getNim());
-            statement.executeUpdate();
-        }catch(Exception e){
-            e.getMessage();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
         }
     }
-     
-     public List<Mahasiswa> select(int skip, int max){
-         ArrayList<Mahasiswa> list=new ArrayList<>();
-         try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(SELECT_SQL)){
-             statement.setInt(1, skip);
-             statement.setInt(2, max);
-             try(ResultSet resultSet=statement.executeQuery()){
-                 while(resultSet.next()){
-                     Mahasiswa mahasiswa=new Mahasiswa();
-                     mahasiswa.setNim(resultSet.getString("nim"));
-                     mahasiswa.setNama(resultSet.getString("nama"));
-                     mahasiswa.setTemptLahir(resultSet.getString("tempat_lahir"));
-                     mahasiswa.setTglLahir(resultSet.getDate("tgl_lahir"));
-                     mahasiswa.setTelepon(resultSet.getString("telepon"));
-                     mahasiswa.setAlamat(resultSet.getString("alamat"));
-                     list.add(mahasiswa);
-                 }
-             }
-         }catch(Exception e){
-             e.getMessage();
-         }finally{
-             return list;
-         }
-     }
-     
-     public Mahasiswa select(String nim){
-         Mahasiswa mahasiswa = null;
-         try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(SELECT_ONE_SQL)){
-             statement.setString(1, nim);
-             try(ResultSet resultSet=statement.executeQuery()){
-                 while(resultSet.next()){
-                     mahasiswa=new Mahasiswa();
-                     mahasiswa.setNim(resultSet.getString("nim"));
-                     mahasiswa.setNama(resultSet.getString("nama"));
-                     mahasiswa.setTemptLahir(resultSet.getString("tempat_lahir"));
-                     mahasiswa.setTglLahir(resultSet.getDate("tgl_lahir"));
-                     mahasiswa.setTelepon(resultSet.getString("telepon"));
-                     mahasiswa.setAlamat(resultSet.getString("alamat"));
-                 }
-             }
-         }catch(Exception e){
-             e.getMessage();
-         }finally{
-             return mahasiswa;
-         }
-     }
-     
-     public Long count(){
-         Long total=0L;
-         try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(SELECT_ONE_SQL)){
-             try(ResultSet resultSet=statement.executeQuery()){
-                 if(resultSet.next()){
-                     total=resultSet.getLong("total");
-                 }
-             }
-         }catch(Exception e){
-             e.getMessage();
-         }finally{
-             return total;
-         }
-     }
+    
+    public void update(Mahasiswa mahasiswa){
+        try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(UPDATE_SQL)){
+           
+            statement.setString(1, mahasiswa.getNama());
+            statement.setString(2, mahasiswa.getTemptLahir());
+            statement.setDate(3, new Date(mahasiswa.getTglLahir().getTime()));
+            statement.setString(4, mahasiswa.getTelepon());
+            statement.setString(5, mahasiswa.getAlamat());
+             statement.setString(6, mahasiswa.getNim());
+            statement.executeUpdate();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void delete(Mahasiswa mahasiswa){
+         try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(DELETE_SQL)){
+             statement.setString(1, mahasiswa.getNim());
+             statement.executeUpdate();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public List<Mahasiswa> select(int skip, int max){
+        ArrayList<Mahasiswa> list=new ArrayList<>();
+        try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(SELECT_SQL)){
+            statement.setInt(1, skip);
+            statement.setInt(2, max);
+            try(ResultSet resultSet=statement.executeQuery()){
+                while(resultSet.next()){
+                    Mahasiswa mahasiswa=new Mahasiswa();
+                    mahasiswa.setNim(resultSet.getString("nim"));
+                    mahasiswa.setNama(resultSet.getString("nama"));
+                    mahasiswa.setTemptLahir(resultSet.getString("tempat_lahir"));
+                    mahasiswa.setTglLahir(resultSet.getDate("tgl_lahir"));
+                    mahasiswa.setTelepon(resultSet.getString("telepon"));
+                    mahasiswa.setAlamat(resultSet.getString("alamat"));
+                    list.add(mahasiswa);
+                }
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }finally{
+            return list;
+        }
+    }
+    
+    public Long count(){
+        Long total=0L;
+        try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(COUNT_SQL);
+                ResultSet resultSet=statement.executeQuery()){
+            if(resultSet.next()){
+                total=resultSet.getLong("total");
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }finally{
+            return total;
+        }
+    }
+    
+    public Mahasiswa select(int kode){
+        Mahasiswa mahasiswa=null;
+        try(PreparedStatement statement=DatabaseHelper.getConnection().prepareStatement(SELECT_ONE_SQL)){
+            statement.setInt(1, kode);
+            try(ResultSet resultSet=statement.executeQuery()){
+                if(resultSet.next()){
+                    mahasiswa=new Mahasiswa();
+                    mahasiswa.setNim(resultSet.getString("nim"));
+                    mahasiswa.setNama(resultSet.getString("nama"));
+                    mahasiswa.setTemptLahir(resultSet.getString("tempat_lahir"));
+                    mahasiswa.setTglLahir(resultSet.getDate("tgl_lahir"));
+                    mahasiswa.setTelepon(resultSet.getString("telepon"));
+                    mahasiswa.setAlamat(resultSet.getString("alamat"));
+                }
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }finally{
+            return mahasiswa;
+        }
+    }
+    
+    
 }
